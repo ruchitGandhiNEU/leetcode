e1852d93b3edfd0b46269d96b3e7372dc0793f29
ruchitGandhiNEU <55202985+ruchitGandhiNEU@users.noreply.github.com>
2021-06-05 15:31:44 -0400

    Hello World!

---
+++ random/test.java
@@ -0,0 +1,9 @@
+public class test {
+    public test() {
+        System.out.println("Testing");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
